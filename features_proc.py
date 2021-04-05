0c021e7bc638d3d7e20d0ca2736e564f45216313
imtth79 <44563899+imtth79@users.noreply.github.com>
2021-04-05 16:37:29 +0700

    Add files via upload

--- features_proc.py
+++ features_proc.py
@@ -1,51 +1,44 @@
 import scipy.io.wavfile as wav
-import speechpy
 import numpy as np 
-import matplotlib.pyplot as plt
+from speechpy.feature import mfcc
+from speechpy.feature import lmfe
+import PIL.Image as img
 import os
+
+dataDirName = "/home/huong/SoundCalculationData"
+resSnoreDir = dataDirName + "/MFCCDatas/Snoring"
+resNonSnoreDir = dataDirName + "/MFCCDatas/NonSnoring"
 
 pathSnoring = "/home/huong/SoundCalculationData/Pre_processData/Snoring"
 pathNonSnoring = "/home/huong/SoundCalculationData/Pre_processData/Non-snoring"    
 
-def loadOneWavFile():
-    fs, sig = wav.read(file_name)
-    sig = sig[:,0]
-    mfcc = speechpy.feature.mfcc(signal, sampling_frequency = fs, frame_length = 0.030, frame_stride = 0.01, num_cepstral = 32,
-                        num_filters = 32, fft_length = 512, low_frequency = 0, high_frequency = None)
-    return mfcc
+if not os.path.exists(resSnoreDir):
+    os.makedirs(resSnoreDir)
 
 
-def loadRawData():
-    label = np.empty((0, 0), dtype=np.uint8)
-    features = np.empty((0,2))
-    num_feat = 0
+if not os.path.exists(resNonSnoreDir):
+    os.makedirs(resNonSnoreDir)
 
-    listSnore = os.listdir(pathSnoring)
-    listNonSnore = os.listdir(pathNonSnoring)
-    
-    num_feat = 0
-    for id in range(0, len(listSnore)): 
-        file_path = os.path.join(pathSnoring, listSnore[id])
-        file_name = os.path.join(file_path, 'snoring.wav')
-        mfcc_feat = loadOneWavFile(file_name)
-        features = np.append(features, mfcc_feat, axis = 0)
-        num_feat += mfcc_feat.shape[0]
-    label = np.append(label, np.zeros(num_feat))
-    
-    num_feat = 0
-    for id in range(0, len(listNonSnore)): 
-        file_path = os.path.join(pathNonSnoring, listNonSnore[id])
-        file_name = os.path.join(file_path, 'nonsnoring.wav')
-        mfcc_feat = loadOneWavFile(file_name)
-        features = np.append(features, mfcc_feat, axis = 0)
-        num_feat += mfcc_feat.shape[0]
-    label = np.append(label, np.zeros(num_feat))
+for filename in os.listdir(pathSnoring):
+    if filename.endswith('.wav'):
+        (fs, sig) = wav.read(pathSnoring + "/" + filename)
 
-    index = list(range(features.shape[0]))
+        #extract mfcc features
+        mfcc_feat = mfcc(sig, sampling_frequency=fs, frame_length=0.030, frame_stride=0.01,
+         num_cepstral=32, num_filters=32, fft_length=512, low_frequency=0, high_frequency=None)
 
-    return index, features, label
+        outputFile = resSnoreDir + "/" + os.path.splitext(filename)[0] + ".jpg"
+        mfcc_data = img.fromarray(mfcc_feat)
+        mfcc_data.save(outputFile)
 
-if __name__ == "__main__":
-    index, features, label = loadRawData()
-    print(label.shape)
-    print(features.shape)
+for filename in os.listdir(pathNonSnoring):
+    if filename.endswith('.wav'):
+        (fs, sig) = wav.read(pathNonSnoring + "/" + filename)
+
+        #extract mfcc features
+        mfcc_feat = mfcc(sig, sampling_frequency=fs, frame_length=0.030, frame_stride=0.01,
+         num_cepstral=32, num_filters=32, fft_length=512, low_frequency=0, high_frequency=None)
+
+        outputFile = resNonSnoreDir + "/" + os.path.splitext(filename)[0] + ".jpg"
+        mfcc_data = img.fromarray(mfcc_feat)
+        mfcc_data.save(outputFile)
